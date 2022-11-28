892aa1e6a233b364addd8d372cafb7e21137db2f
Th-Hatake <96919733+Th-Hatake@users.noreply.github.com>
2022-11-27 21:23:21 -0300

    Add files via upload
    commit inicial

---
+++ app/js/satelites.js
@@ -0,0 +1,59 @@
+function satelite(id, name, coordX, coordY, noradId, sensor){
+    this.id = id;
+    this.name = name;
+    this.coordX = coordX.toFixed(4);
+    this.coordY = coordY.toFixed(4);
+    this.noradId = noradId;
+    this.sensor = sensor;
+    this.elevation = 0;
+    this.azimuth = 0;
+    
+    this.print = () => {
+        return `
+        <div class="row mt-2 mb-2">
+            <div class="col-lg-4 col-md-2 col-sm-2 col-xs-1">${this.id}</div>
+            <div class="col-lg-7 col-md-9 col-sm-9 col-xs-9"><i class="fa-solid fa-satellite" style="color:black;"></i> ${this.name}</div>
+            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-2"><a href="/satelite.html?sat=${this.id}" class="btn btn-info btn-xs">+info</a></div>
+        </div>`;
+
+    }
+}
+async function getSatelites(){
+    const satelites = [];
+
+    const data = await (await fetch("https://api.spectator.earth/satellite/")).json();
+    data.features.forEach(x => {
+        satelites.push(new satelite(
+            x.id,
+            x.properties.name,
+            x.geometry ? x.geometry.coordinates[0] : 'não disponível',
+            x.geometry ? x.geometry.coordinates[1] : 'não disponível',
+            x.properties.norad_id,
+            x.sensors ? x.sensors[0].type : 'não disponível'
+        ))}
+    );     
+    return satelites
+} 
+async function getSatelite(id){
+    const data = await (await fetch("https://api.spectator.earth/satellite/"+id)).json();
+
+    const sat = new satelite(
+        data.id,
+        data.properties.name,
+        data.geometry ? data.geometry.coordinates[0] : 'não disponível',
+        data.geometry ? data.geometry.coordinates[1] : 'não disponível',
+        data.properties.norad_id,
+        data.sensors ? data.sensors[0].type : 'não disponível'
+    );
+    
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position)=>{
+            let posicao = position.coords;
+            // return posicao;
+        });
+    }else{
+        alert('erro: não foi possível acessar a localização detalhada');
+    }
+
+    return sat;
+}
